962d3c85b64c65c1edd689b5353f6938d0ebbba2
Anton Averkiev <wowgamr@yandex.ru>
2020-03-04 16:58:39 +0300

    Update code style

--- checkout_links/code.js
+++ checkout_links/code.js
@@ -1,11 +1,12 @@
-$(function(){
-    if (location.pathname == '/new_order') {
-        $.get('/cart_items.json')
-            .done(function (cart) {
-            $.each(cart.order_lines, function(){
-            console.log('[' + this.product_id + '] ' + this.title);
-            $('div.co-basket_item-description:contains("' + this.title + '")').html('<a href="/product_by_id/' + this.product_id + '" target="_blank" class="co-basket_item-link">' + this.title + '</a>');
-            });
-        });
-    };
+$(function () {
+	if (location.pathname == '/new_order') {
+	$.get('/cart_items.json')
+		.done(function (cart) {
+		$.each(cart.order_lines, function () {
+			console.log('[' + this.product_id + '] ' + this.title);
+			$('div.co-basket_item-description:contains("' + this.title + '")').html('<a href="/product_by_id/' + this.product_id + '" target="_blank" class="co-basket_item-link">' + this.title + '</a>');
+		});
+		});
+	};
 });
+	
